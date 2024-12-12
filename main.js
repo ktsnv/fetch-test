383077f55cdc911fad62bfa0ba3c1c5a0096110b
Henry Lee <104782108+ktsnv@users.noreply.github.com>
2024-12-12 14:15:48 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+let data = fetchData('data.txt');
+console.log(data);
+function fetchData(url) {
+  let temp;
+  fetch(url)
+    .then(res=>res.text()) 
+    .then(text => { 
+      temp = text.split("\n") 
+    };
+  return temp;
+}
